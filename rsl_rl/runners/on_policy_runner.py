5c81c368496fdf884a148106a6897789b36d9391
jnskkmhr <junjun.k2012@gmail.com>
2026-06-24 17:53:56 -0500

    initialize logger in runner constructor

--- rsl_rl/runners/on_policy_runner.py
+++ rsl_rl/runners/on_policy_runner.py
@@ -53,6 +53,9 @@ class OnPolicyRunner:
 
         self.current_learning_iteration = 0
 
+        # Initialize the logging writer
+        self.logger.init_logging_writer()
+
     def learn(self, num_learning_iterations: int, init_at_random_ep_len: bool = False) -> None:
         """Run the learning loop for the specified number of iterations."""
         # Randomize initial episode lengths (for exploration)
@@ -69,9 +72,6 @@ class OnPolicyRunner:
         if self.is_distributed:
             print(f"Synchronizing parameters for rank {self.gpu_global_rank}...")
             self.alg.broadcast_parameters()
-
-        # Initialize the logging writer
-        self.logger.init_logging_writer()
 
         # Start training
         start_it = self.current_learning_iteration
