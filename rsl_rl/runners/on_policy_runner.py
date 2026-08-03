ed3fcbbbeb54922ce04a1b88a13b41b3a2abc97f
jnskkmhr <junjun.k2012@gmail.com>
2026-08-03 11:13:48 -0500

    set external_data=False for onnx export

--- rsl_rl/runners/on_policy_runner.py
+++ rsl_rl/runners/on_policy_runner.py
@@ -209,6 +209,7 @@ class OnPolicyRunner:
             verbose=verbose,
             input_names=onnx_model.input_names,  # type: ignore
             output_names=onnx_model.output_names,  # type: ignore
+            external_data=False,
         )
 
     def add_git_repo_to_log(self, repo_file_path: str) -> None:
